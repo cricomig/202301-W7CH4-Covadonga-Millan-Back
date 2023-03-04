020d09f9febfd6e9e0cd13f3fe4759a082681019
Covadonga M <cova.millan@gmail.com>
2023-03-04 15:35:41 +0100

    Add some files

--- src/routers/jokes.router.ts
+++ src/routers/jokes.router.ts
@@ -6,22 +6,22 @@ import { JokesMongoRepo } from '../repository/jokes/jokes.mongo.repo.js';
 import { UsersMongoRepo } from '../repository/users/users.mongo.repo.js';
 
 export const jokesRouter = Router();
-const jokesRepo = new JokesMongoRepo();
-const usersRepo = new UsersMongoRepo();
+const jokesRepo = JokesMongoRepo.getInstance();
+const usersRepo = UsersMongoRepo.getInstance();
 const controller = new JokesController(jokesRepo, usersRepo);
 
-jokesRouter.get('/', authentication, controller.getAll.bind(controller));
-jokesRouter.get('/:id', authentication, controller.get.bind(controller));
+jokesRouter.get('/', controller.getAll.bind(controller));
+jokesRouter.get('/:id', controller.get.bind(controller));
 jokesRouter.post('/', authentication, controller.post.bind(controller));
 jokesRouter.patch(
   '/:id',
   authentication,
-  authorization,
+  (req, resp, next) => authorization(req, resp, next, jokesRepo),
   controller.patch.bind(controller)
 );
 jokesRouter.delete(
   '/:id',
   authentication,
-  authorization,
+  (req, resp, next) => authorization(req, resp, next, jokesRepo),
   controller.delete.bind(controller)
 );
--- src/routers/users.router.ts
+++ src/routers/users.router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { UsersController } from '../controllers/users/users.controller.js';
 import { UsersMongoRepo } from '../repository/users/users.mongo.repo.js';
+// const debug = createDebug('W6:router:users');
 
 export const usersRouter = Router();
-const repo = new UsersMongoRepo();
+// debug('Loaded');
+const repo = UsersMongoRepo.getInstance();
 const controller = new UsersController(repo);
 
+usersRouter.get('/', controller.getAll.bind(controller));
 usersRouter.post('/register', controller.register.bind(controller));
 usersRouter.post('/login', controller.login.bind(controller));
